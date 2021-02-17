8440105e8c444c116f24aaea55848dd5000d6181
Dylan Hayes <dhayes@uwhealth.org>
2021-02-17 14:57:14 -0600

    Initial Commit

---
+++ VSProjectRenamer.ps1
@@ -0,0 +1,13 @@
+﻿$dir = Read-Host "Project Directory"
+$token = Read-Host "Name of original project"
+$newtoken = Read-Host "Name of new project"
+
+#renames all items with your newtoken that matches the old token
+get-childitem -Path "$dir" -Recurse | where-object { $_.Name -like "*" + $token + "*" } | Rename-Item -NewName {$_.name -replace $token, $newtoken} 
+
+#gets all files not in the bin or obj folders
+get-childitem -Path "$dir" -Recurse | where { ! $_.PSIsContainer -and $_.FullName -notmatch '\\bin\\' -and $_.FullName -notmatch '\\obj\\' } |
+
+Foreach-Object {  
+    (Get-Content -path $_.FullName) -replace $token, $newtoken | Set-Content -Path $_.FullName
+}
